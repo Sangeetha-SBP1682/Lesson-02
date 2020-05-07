c5a0186d67da18ec9336f6690e2a626015289a3d
Sangeetha-SBP1682 <sangeetha.sbp@gmail.com>
2020-05-07 13:35:50 +0000

    I am pushing all files to Github

---
+++ PP1.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+<script type ="text/javascript">
+function msg(){
+alert("Hello World from Javascript file");
+}
+</script>
+</head>
+<body>
+<p>Welcome to  Javacript </p>
+<form>
+<input type="button" value="click" onclick="msg()">
+	
+</input>
+</form>
+</body>
