7b2499b5afdd995e3f1921e181d172b5ab42e319
Sangeetha-SBP1682 <sangeetha.sbp@gmail.com>
2020-05-08 14:21:33 +0000

    Another Commit

--- PP1.java
+++ PP1.java
@@ -2,6 +2,7 @@ class HelloJava
 {
 public static void main(String args[])
     {
-      System.out.println("Hello World from Java");
+      System.out.println("Hello World from Java ");
+      System.out.println("Change are way of life");
     }
 }
