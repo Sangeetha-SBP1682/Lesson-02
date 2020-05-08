2d60222ca78ebdca5bf05ec06d524dde3d84f1ae
Sangeetha-SBP1682 <sangeetha.sbp@gmail.com>
2020-05-08 19:05:17 +0000

    Commmit f Email Notification

--- PP1.java
+++ PP1.java
@@ -4,5 +4,6 @@ public static void main(String args[])
     {
       System.out.println("Hello World from Java ");
       System.out.println("Change are way of life");
+      System.out.printn ("Check Email Notification on Build Job Run  Jenkins");
     }
 }
